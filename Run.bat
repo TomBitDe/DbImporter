8497f4932ae67ba5988798c65340c14ef84d84a0
TomBitDe <tbit455674@gmail.com>
2023-05-13 11:28:09 +0200

    Refactored.

--- Run.bat
+++ Run.bat
@@ -9,10 +9,10 @@ set PATH=C:\"Program Files"\Java\jdk-14.0.1\bin;%PATH%
 java -version
 
 REM For debugging log4j2 enable the following line
-REM set LOG4J_OPTIONS=-Dlog4j.debug -Dlog4j.configurationFile=file:///C:/Users/Tom/Entwicklung/Java/Test/Tests/DbImporter/log4j2.xml
+REM set LOG4J_OPTIONS=-Dlog4j.debug -Dlog4j.configurationFile=file:./log4j2.xml
 
 REM This is no debugging log4j2
-set LOG4J_OPTIONS=-Dlog4j.configurationFile=file:///C:/Users/Tom/Entwicklung/Java/Test/Tests/DbImporter/log4j2.xml
+set LOG4J_OPTIONS=-Dlog4j.configurationFile=file:./log4j2.xml
 
 java %JMX_OPTIONS% %LOG4J_OPTIONS% -classpath ./target/DbImporter-jar-with-dependencies.jar com.home.dbimportermaven.dbimporter.DbImporter 
 
